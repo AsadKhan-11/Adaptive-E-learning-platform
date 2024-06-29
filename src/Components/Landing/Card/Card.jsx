e462d343ffd6782bd9ced292e1cbd3a88bac3980
Asad Khan <mrasad10khan@gmail.com>
2024-06-29 14:27:39 +0500

    Creating landing card

--- src/Components/Landing/Card/Card.jsx
+++ src/Components/Landing/Card/Card.jsx
@@ -13,19 +13,39 @@ function Card() {
     <div className="Landing-card">
       <ReactCardFlip flipDirection="horizontal" isFlipped={isFlipped}>
         <div className="card" onClick={Flipped}>
-          <label htmlFor="">Name</label>
-          <input className="card-input" />
+          <h1 className="card-header">Login</h1>
 
-          <label htmlFor="">Password</label>
-          <input className="card-input" />
+          <div className="card-info">
+            <label htmlFor="card-label">Email</label>
+            <input className="card-input" />
+          </div>
+
+          <div className="card-info">
+            <label htmlFor="card-label">Password</label>
+            <input className="card-input" />
+          </div>
+
+          <button className="card-btn">Login</button>
         </div>
 
         <div className="card card-back" onClick={Flipped}>
-          <label htmlFor="">Name</label>
-          <input className="card-input" />
+          <h1 className="card-header">Sign up</h1>
+          <div className="card-info">
+            <label htmlFor="card-label">Email</label>
+            <input className="card-input" />
+          </div>
 
-          <label htmlFor="">Password</label>
-          <input className="card-input" />
+          <div className="card-info">
+            <label htmlFor="card-label">Password</label>
+            <input className="card-input" />
+          </div>
+
+          <div className="card-info">
+            <label htmlFor="card-label">Phone Number</label>
+            <input className="card-input" />
+          </div>
+
+          <button className="card-btn">Sign up</button>
         </div>
       </ReactCardFlip>
     </div>
