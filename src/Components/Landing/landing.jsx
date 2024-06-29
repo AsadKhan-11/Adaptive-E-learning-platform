4e377d0a72f2f7f6b0fa98ea379addd631d1e311
Asad Khan <mrasad10khan@gmail.com>
2024-06-30 03:01:18 +0500

    Creating landing page

--- src/Components/Landing/landing.jsx
+++ src/Components/Landing/landing.jsx
@@ -5,6 +5,14 @@ import Card from "./Card/Card";
 function landing() {
   return (
     <div className="Landing-page">
+      <h1 className="landing-header">
+        Unlock Your Potential with Personalized AI Learning
+      </h1>
+      {/* <div class="line"></div>
+      <div class="line"></div>
+      <div class="line"></div>
+      <div class="line"></div>
+      <div class="line"></div> */}
       <Card />
     </div>
   );
