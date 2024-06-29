927d200461d97a01f2aeacd0a5ab44183cc3bd8c
Asad Khan <mrasad10khan@gmail.com>
2024-06-29 15:18:55 +0500

    Creating Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
 import Landing from "./Components/Landing/landing";
 import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div className="App">
+      <Navbar />
       <Landing />
-    </>
+    </div>
   );
 }
 
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Navbar.css";
+function Navbar() {
+  return (
+    <div className="Navbar-sec">
+      <h1 className="nav-header">Learn Genie</h1>
+
+      <nav className="nav-link-wrapper">
+        <a href="" className="nav-link">
+          Home
+        </a>
+        <a href="" className="nav-link">
+          Courses
+        </a>
+        <a href="" className="nav-link">
+          Progress
+        </a>
+        <a href="" className="nav-link">
+          Help
+        </a>
+        <a href="" className="nav-link">
+          Profile
+        </a>
+      </nav>
+      <a href="" className="nav-sign">
+        Sign-up / Login
+      </a>
+    </div>
+  );
+}
+
+export default Navbar;
