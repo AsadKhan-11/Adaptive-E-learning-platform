80b4a9228f6528d43249df1f2487a0be93bbcb17
Asad Khan <mrasad10khan@gmail.com>
2024-07-02 12:11:57 +0500

    Editing quiz page

--- src/Components/Course/Level/Select/Quiz/Quiz.jsx
+++ src/Components/Course/Level/Select/Quiz/Quiz.jsx
@@ -25,28 +25,9 @@ function Quiz() {
             <input type="radio" />
             <p className="quiz-answer">To add background images</p>
           </div>
-        </div>
-
-        <div className="quiz-container">
-          <h3 className="quiz-question">What is grid used for?</h3>
-          <div className="quiz-detail">
-            <input type="radio" />
-            <p className="quiz-answer">To create 3D animations</p>
-          </div>
-          <div className="quiz-detail">
-            <input type="radio" />
-            <p className="quiz-answer">To style text and fonts</p>
-          </div>
-          <div className="quiz-detail">
-            <input type="radio" />
-            <p className="quiz-answer">
-              To layout and align items in a container
-            </p>
-          </div>
-          <div className="quiz-detail">
-            <input type="radio" />
-            <p className="quiz-answer">To add background images</p>
-          </div>
+          <button type="submit" className="quiz-btn">
+            Submit
+          </button>
         </div>
       </div>
     </div>
