6f24a58e4f2225c8e8170a4b8c52a7aa63d72213
Asad Khan <mrasad10khan@gmail.com>
2024-10-22 11:58:01 +0500

    Minor Adjustments

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import Nexedu from "../../assets/NEXEDU.png";
 import "./Navbar.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function Navbar({ navText, handleNavClick }) {
+  const location = useLocation();
+
+  const isLoginPage = location.pathname === "/";
+
   return (
     <div
-      className={
-        navText === "Signup" || navText === "Login"
-          ? "Navbar-sec nav-first"
-          : "Navbar-sec"
-      }
+      className={`Navbar-sec ${isLoginPage ? "nav-first" : " "}
+          
+      `}
     >
       <div className="nav-container">
         <img src={Nexedu} alt="" className="nav-logo" />
