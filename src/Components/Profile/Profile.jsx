40a65d2bafad8fb84a81c2988939cc9a38a4215d
Asad Khan <mrasad10khan@gmail.com>
2024-10-25 18:43:54 +0500

    Adding buttons

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,33 +1,100 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Profile.css";
 function Profile() {
+  const [formData, setFormData] = useState({
+    name: "Asad Ahmed Khan",
+    email: "ak4786083@gmail.com",
+    phone: "03104388534",
+    active: "1",
+  });
+
+  const [isEditable, setIsEditable] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const toggleEdit = (e) => {
+    e.preventDefault();
+    setIsEditable(!isEditable);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsEditable(false);
+  };
+
   return (
     <div className="profile">
-      <form className="profile-form" name="profile-form">
+      <form className="profile-form" name="profile-form" onSubmit={toggleEdit}>
         <div className="profile-info">
           <label htmlFor="" className="profile-label">
             Name
           </label>
-          <h3 className="profile-input">Asad Ahmed Khan</h3>
+          <input
+            className={isEditable ? "editable" : "disabled"}
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            readOnly={!isEditable}
+          />{" "}
         </div>
         <div className="profile-info">
           <label htmlFor="" className="profile-label">
             Email
           </label>
 
-          <h3 className="profile-input">ak4786083@gmail.com</h3>
+          <input
+            className="disabled"
+            disabled
+            type="text"
+            name="email"
+            onChange={handleChange}
+            readOnly={!isEditable}
+            value={formData.email}
+          />
         </div>
         <div className="profile-info">
           <label htmlFor="" className="profile-label">
             Phone
           </label>
-          <h3 className="profile-input">03104388534</h3>
+          <input
+            className="disabled"
+            disabled
+            type="text"
+            name="phone"
+            onChange={handleChange}
+            readOnly={!isEditable}
+            value={formData.phone}
+          />{" "}
         </div>
         <div className="profile-info">
           <label htmlFor="" className="profile-label">
-            Active Courses
+            Courses
           </label>
-          <h3 className="profile-input">1</h3>
+          <input
+            className="disabled"
+            disabled
+            type="text"
+            name="active"
+            value={formData.active}
+          />
+        </div>
+
+        <div className="profile-btns">
+          <button className="profile-change nav-sign-btn" onClick={toggleEdit}>
+            {isEditable ? "Cancel" : "Edit"}
+          </button>{" "}
+          {isEditable && (
+            <button
+              className="profile-submit nav-sign-btn"
+              onClick={handleSubmit}
+            >
+              Submit
+            </button>
+          )}
         </div>
       </form>
     </div>
