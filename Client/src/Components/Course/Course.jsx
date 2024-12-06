852ea2d5db986ec8f22ebd758c7f840c2d0149d4
Asad Khan <mrasad10khan@gmail.com>
2024-12-07 00:15:11 +0500

    Adding images to courses

--- Client/src/Components/Course/Course.jsx
+++ Client/src/Components/Course/Course.jsx
@@ -1,15 +1,31 @@
 import React from "react";
 import "./Course.css";
-import image from "../../assets/Background.jpg";
+import html from "./images/html.png";
+import css from "./images/css.jpg";
+import js from "./images/js.png";
 import { Link } from "react-router-dom";
 function Course() {
   return (
     <div className="course">
       <Link to="/course/enrollment" className="course-link">
         <div className="course-container">
-          <img src={image} className="course-image" />
-          <h2 className="course-name">Web development course</h2>
-          <p className="course-duration">Duration 3 months</p>
+          <img src={html} className="course-image" />
+          <h2 className="course-name">HTML course</h2>
+          <p className="course-duration">Duration 1 month</p>
+        </div>
+      </Link>
+      <Link to="/course/enrollment" className="course-link">
+        <div className="course-container">
+          <img src={css} className="course-image" />
+          <h2 className="course-name">CSS course</h2>
+          <p className="course-duration">Duration 2 months</p>
+        </div>
+      </Link>
+      <Link to="/course/enrollment" className="course-link">
+        <div className="course-container">
+          <img src={js} className="course-image" />
+          <h2 className="course-name">JavaScript course</h2>
+          <p className="course-duration">Duration 4 months</p>
         </div>
       </Link>
     </div>
--- Client/src/Components/Course/Enrollment/Enroll.jsx
+++ Client/src/Components/Course/Enrollment/Enroll.jsx
@@ -1,24 +1,21 @@
 import React from "react";
 import "./Enroll.css";
-import img from "../../../assets/Background.jpg";
-
+import html2 from "../images/html2.jpg";
 function Enroll() {
   return (
     <div className="enroll">
       <div className="enroll-container">
         <p className="enroll-desc">
           This course is designed to teach you how to create amazing websites
-          from scratch. You will start by learning HTML, the language used to
-          structure content on a webpage. Then, you’ll move on to CSS, which is
-          used to style your websites and make them look beautiful with colors,
-          layouts, and designs. Finally, you’ll dive into JavaScript, the
-          language that adds interactivity to your websites, like buttons that
-          respond when clicked or animations that make your site engaging. By
-          the end of this course, you’ll be able to build your own professional
-          websites!
+          from scratch using HTML. You will start by learning the basics of
+          HTML, the language used to structure content on a webpage. You’ll
+          discover how to create headings, paragraphs, lists, links, images, and
+          more. By the end of this course, you’ll have a strong foundation in
+          HTML and will be able to structure professional web pages with
+          confidence!
         </p>
 
-        <img src={img} alt="enrollment image" className="enroll-img" />
+        <img src={html2} alt="enrollment image" className="enroll-img" />
       </div>
       <button className="enroll-btn">Enroll</button>
     </div>
