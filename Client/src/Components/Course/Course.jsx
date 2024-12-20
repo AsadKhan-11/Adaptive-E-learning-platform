cd89c71eea252de5bb5b78f867385767f9d40390
Asad Khan <mrasad10khan@gmail.com>
2024-12-20 23:41:04 +0500

    Fixing Courses

--- Client/src/Components/Course/Course.jsx
+++ Client/src/Components/Course/Course.jsx
@@ -1,33 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Course.css";
 import html from "./images/html.png";
 import css from "./images/css.jpg";
 import js from "./images/js.png";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import data from "./Data";
 function Course() {
+  const handleClick = async (courseId) => {
+    const token = localStorage.getItem("token");
+    const response = axios.get(`http://localhost:3000/api/course/${courseId}`);
+  };
+
   return (
     <div className="course">
-      <Link to="/course/enrollment" className="course-link">
-        <div className="course-container">
-          <img src={html} className="course-image" />
-          <h2 className="course-name">HTML course</h2>
-          <p className="course-duration">Duration 1 month</p>
-        </div>
-      </Link>
-      <Link to="/course/enrollment" className="course-link">
-        <div className="course-container">
-          <img src={css} className="course-image" />
-          <h2 className="course-name">CSS course</h2>
-          <p className="course-duration">Duration 2 months</p>
-        </div>
-      </Link>
-      <Link to="/course/enrollment" className="course-link">
-        <div className="course-container">
-          <img src={js} className="course-image" />
-          <h2 className="course-name">JavaScript course</h2>
-          <p className="course-duration">Duration 4 months</p>
-        </div>
-      </Link>
+      {data.map((course) => (
+        <Link
+          to="/course/enrollment"
+          className="course-link"
+          key={course.courseId}
+          onClick={handleClick(course.courseId)}
+        >
+          <div className="course-container">
+            <img src={course.courseImg} className="course-image" />
+            <h2 className="course-name">{course.courseTitle}</h2>
+            <p className="course-duration">{course.courseDuration}</p>
+          </div>
+        </Link>
+      ))}
     </div>
   );
 }
---
+++ Client/src/Components/Course/Data.js
@@ -0,0 +1,29 @@
+import img1 from "./images/html.png";
+import img2 from "./images/css.jpg";
+import img3 from "./images/js.png";
+
+const data = [
+  {
+    courseId: "html-basics",
+    courseTitle: "HTML course",
+    courseDesc: "",
+    courseImg: img1,
+    courseDuration: "1 month",
+  },
+  {
+    courseId: "advance-css",
+    courseTitle: "CSS course",
+    courseDesc: "",
+    courseImg: img2,
+    courseDuration: "2 months",
+  },
+  {
+    courseId: "js-pro",
+    courseTitle: "Javascript course",
+    courseDesc: "",
+    courseImg: img3,
+    courseDuration: "4 months",
+  },
+];
+
+export default data;
