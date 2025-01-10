9aeff441a598e03c7f0979da596444bccb80be41
Abdul Ahad <110593806+ahad51@users.noreply.github.com>
2025-01-10 13:01:41 +0500

    Create index.js at root

---
+++ index.js
@@ -0,0 +1,26 @@
+// Required Modules
+const AuthRouter = require("./Routes/AuthRoute");
+const ProtectedRoute = require("./Routes/ProtectedRoute");
+const EmailRoute = require("./Routes/EmailRoute");
+require("./Model/db");
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+// Initialize Express app
+const app = express();
+
+// Middleware setup
+app.use(express.json()); 
+app.use(cors()); 
+
+// Define Routes
+app.use("/auth", AuthRouter);
+app.use("/api", ProtectedRoute);
+app.use("/email", EmailRoute);
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
